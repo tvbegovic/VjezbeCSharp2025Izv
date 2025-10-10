fb3d8918d12912a081cffaba1f2e595106b99d95
midomlijavern <midomlija@vernnet.hr>
2025-10-10 20:12:23 +0200

    Add project files.

---
+++ Inicijali/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Ime: ");
+string ime = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(ime))
+{
+    Console.WriteLine("Ime ne smije biti prazno.");
+    return;
+}
+
+Console.Write("Prezime: ");
+string prezime = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(prezime))
+{
+    Console.WriteLine("Prezime ne smije biti prazno.");
+    return;
+}
+
+Console.WriteLine($"Vaši inicijali su {ime.Substring(0, 1)}.{prezime.Substring(0, 1)}.");
---
+++ TocnoVrijeme/Program.cs
@@ -0,0 +1,4 @@
+﻿//Stari način zamjene varijable
+Console.WriteLine("Dobar dan. Točno vrijeme je: {0}", DateTime.Now);
+//novi način zamjene varijable
+Console.WriteLine($"Dobar dan. Točno vrijeme je: {DateTime.Now}");
