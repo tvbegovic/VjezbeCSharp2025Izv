f522e4bacb8990e41b51abe42884b9d86259acd7
midomlijavern <midomlija@vernnet.hr>
2025-10-17 20:06:40 +0200

    prosjek ocjena

---
+++ ProsjekOcjena/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+	Console.Write("Ocjena C: ");
+	string unos = Console.ReadLine();
+	int ocjenaC = int.Parse(unos);
+	Console.Write("Ocjena C++: ");
+	unos = Console.ReadLine();
+	int ocjenaCPlus = int.Parse(unos);
+	Console.Write("Ocjena C#: ");
+	unos = Console.ReadLine();
+	int ocjenaCSharp = int.Parse(unos);
+	double prosjek = (ocjenaC + ocjenaCPlus + ocjenaCSharp) / 3.0;
+	Console.WriteLine($"Prosjek ocjena je {prosjek:N2}");
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"Dogodila se pogreška. Pokušajte ponovno. Tekst greške: {ex.Message}");
+}
+
+
---
+++ ProsjekOcjena_TryParse/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Ocjena C: ");
+string unos = Console.ReadLine();
+bool ok = int.TryParse(unos, out int ocjenaC);
+if(!ok)
+{
+    Console.WriteLine("Pogrešan format broja");
+    return;
+}
+Console.Write("Ocjena C++: ");
+unos = Console.ReadLine();
+ok = int.TryParse(unos,out int ocjenaCPlus);
+if (!ok)
+{
+    Console.WriteLine("Pogrešan format broja");
+    return;
+}
+Console.Write("Ocjena C#: ");
+unos = Console.ReadLine();
+ok = int.TryParse(unos, out int ocjenaCSharp);
+if (!ok)
+{
+    Console.WriteLine("Pogrešan format broja");
+    return;
+}
+double prosjek = (ocjenaC + ocjenaCPlus + ocjenaCSharp) / 3.0;
+Console.WriteLine($"Prosjek ocjena je {prosjek:N2}");
