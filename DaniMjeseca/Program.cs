720ea628989f9bb946a10972d8eaaa2969083259
tvbegovic <prof@KAT42>
2025-10-24 20:07:39 +0200

    Naredbe odluke

---
+++ DaniMjeseca/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Design;
+
+Console.Write("Unesite redni broj mjeseca (1-12): ");
+string unos = Console.ReadLine();
+bool ok = int.TryParse(unos, out int mjesec);
+if(!ok)
+{
+    Console.WriteLine("Pogrešan format");
+    return;
+}
+if(mjesec < 1 || mjesec > 12)
+{
+    Console.WriteLine("Mjesec je izvan raspona");
+    return;
+}
+int brojDana;
+string dani = "dana";
+switch(mjesec)
+{
+    case 2:
+        if(DateTime.IsLeapYear(DateTime.Now.Year))
+            brojDana = 29;
+        else
+            brojDana = 28;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        brojDana = 30;
+        break;
+    default:
+        brojDana = 31;
+        dani = "dan";
+        break;
+}
+Console.WriteLine($"{mjesec}. mjesec ima {brojDana} {dani}");
---
+++ DatumiRodenja/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.Write("Datum 1: ");
+string unos = Console.ReadLine();
+bool ok = DateTime.TryParse(unos, out DateTime datum1);
+if(!ok)
+{
+    Console.WriteLine("Pogrešan format datuma");
+    return;
+}
+Console.Write("Datum 2: ");
+unos = Console.ReadLine();
+ok = DateTime.TryParse(unos, out DateTime datum2);
+if (!ok)
+{
+    Console.WriteLine("Pogrešan format datuma");
+    return;
+}
+if (datum1 < datum2)
+    Console.WriteLine("Osoba 1 je starija od osobe 2");
+else if (datum2 < datum1)
+    Console.WriteLine("Osoba 2 je starija od osobe 1");
+else
+{
+    Console.WriteLine("Osobe su rođene na isti dan");
+}
+if (datum1.Month < datum2.Month)
+    Console.WriteLine("Osoba 1 slavi rođendan prije osobe 2");
+else if (datum2.Month < datum1.Month)
+    Console.WriteLine("Osoba 2 slavi rođendan prije osobe 1");
+else if (datum1.Day < datum2.Day)
+    Console.WriteLine("Osoba 1 slavi rođendan prije osobe 2");
+else if (datum2.Day < datum1.Day)
+    Console.WriteLine("Osoba 2 slavi rođendan prije osobe 1");
+else
+    Console.WriteLine("Osobe slave rođendan isti dan");
