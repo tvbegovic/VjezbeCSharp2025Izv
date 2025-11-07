5610f2c2dd3424a4bcb0b57abeca4ccd38a5e1fb
tvbegovic <prof@KAT42>
2025-11-07 20:19:51 +0100

    funkcije

---
+++ IzracunKamata/Program.cs
@@ -0,0 +1,51 @@
+﻿double IznosKamata(double iznos, double stopa)
+{
+    if (stopa < 0 || stopa > 20)
+        throw new ArgumentOutOfRangeException("Stopa je izvan raspona");
+    return iznos * stopa / 100;
+}
+
+bool nastavi = true;
+do
+{
+    try
+    {
+        Console.Write("Iznos: ");
+        string unos = Console.ReadLine();
+        if (string.IsNullOrEmpty(unos))
+        {
+            nastavi = false;
+        }
+        else
+        {
+            bool ok = double.TryParse(unos, out double iznos);
+            if (!ok)
+            {
+                Console.WriteLine("Pogrešan format");
+                continue;
+            }
+            Console.Write("Stopa (0-20%): ");
+            unos = Console.ReadLine();
+            ok = double.TryParse(unos, out double stopa);
+            if (!ok)
+            {
+                Console.WriteLine("Pogrešan format");
+                continue;
+            }
+            //provjera prije uvođenja exceptiona
+            /*if (stopa < 0 || stopa > 20)
+            {
+                Console.WriteLine("Stopa je izvan raspona");
+                continue;
+            }*/
+            double kamata = IznosKamata(iznos, stopa);
+            Console.WriteLine($"Za iznos {iznos:N2} i stopu {stopa}% kamata je {kamata:N2}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Dogodila se pogreška. Tekst: {ex.Message}");
+    }
+    
+}
+while (nastavi);
---
+++ OtplataKredita/Program.cs
@@ -0,0 +1,60 @@
+﻿void IzracunParametara(double iznos, double stopa, int brojGodina, out double anuitet, out double iznosKamata)
+{
+    if (iznos < 0 || stopa < 0 || brojGodina < 0)
+        throw new ArgumentException("Parametri moraju biti pozitivni brojevi");
+    double r = Kamatnjak(stopa);
+    int n = brojGodina * 12;
+    anuitet = iznos * Math.Pow(r,n) * (r -1) / (Math.Pow(r,n) - 1);
+    iznosKamata = anuitet * n - iznos;
+}
+
+double Kamatnjak(double stopa)
+{
+    return 1 + stopa / 100 / 12;
+}
+
+bool nastavi = true;
+do
+{
+    try
+    {
+        Console.Write("Iznos: ");
+        string unos = Console.ReadLine();
+        if (string.IsNullOrEmpty(unos))
+            nastavi = false;
+        else
+        {
+            bool ok = double.TryParse(unos, out double iznos);
+            if (!ok)
+            {
+                Console.WriteLine("Pogrešan format");
+                continue;
+            }
+            Console.Write("Stopa: ");
+            unos = Console.ReadLine();
+            ok = double.TryParse(unos, out double stopa);
+            if (!ok)
+            {
+                Console.WriteLine("Pogrešan format");
+                continue;
+            }
+            Console.Write("Broj godina: ");
+            unos = Console.ReadLine();
+            ok = int.TryParse(unos, out int godina);
+            if (!ok)
+            {
+                Console.WriteLine("Pogrešan format");
+                continue;
+            }
+            IzracunParametara(iznos, stopa, godina, out double anuitet, out double ukupnoKamata);
+            Console.WriteLine($"Za iznos {iznos:N2}, stopu {stopa}% i broj godina {godina} anuitet je {anuitet:N2} a ukupna kamata je {ukupnoKamata:N2}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Dogodila se pogreška. Tekst: {ex.Message}");
+    }
+}
+while (nastavi);
+
+
